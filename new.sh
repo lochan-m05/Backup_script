4055694e0461b6d35cd9e5e636d5dd51b522dc35
lochan <m.lochan11@outlook.in>
2025-11-25 23:19:16 +0530

    BackUp file

--- new.sh
+++ new.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
-read -p "Enter your favorite color: " color
 
-if [[ -z "$color" ]]; then
-   echo "You didn't enter anything !"
-else 
-   echo "Great, you entered $color"
+if [ $# -ne 2 ]
+then
+    echo "Usage : Backup.sh <Scource_dir> <Target_dir>"
+    echo "Please try again ."
+    exit 1
 fi
+
+if ! command -v rsync > /dev/null 2>&1
+then
+    echo "This  script requires rsync to be installed."
+    echo "Please use your distribution's package manager to install"
+    exit 2
+fi
+
+current_date=$(date %Y-%m-%d)
+
+rsync_option="-avb --backup-dir $2/$current_date --delete --dry-run"
+$(which rsync) $rsync_option $1 $2/current >> backup_$current_date.log
+
+
 
